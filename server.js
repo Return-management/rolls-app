6d9916b398fc38670bbf735ac86224b306be7658
Return-management <babtou64@gmail.com>
2026-02-25 06:52:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-const db = new sqlite3.Database("/data/database.db");
+const db = new sqlite3.Database("/data/rolls.db");
 
 // ------------------------------------------------------------
 // INITIALISATION DES TABLES
@@ -201,4 +201,5 @@ app.get("/api/historique", (req, res) => {
 
 // ------------------------------------------------------------
 app.listen(PORT, () => console.log("Serveur démarré sur le port " + PORT));
+
 
